b8b39fdcea99d5526cc9d836d27aaf9fe66928f7
2framesper <xloliver222@gmail.com>
2025-08-16 09:22:06 -0400

    Add functionality for all nums / basic operations
    Implement event listener by selecting the button container, and returning if the button isn't pressed
    If statements within the event listener dictate the calculations, for each button type. Any calculations will set the past calculation into pastCalc
    A number button click will check if the first number is complete, if not, add that digit to the number. If it is, then add the digit to the second number
    
    An operator button click will check if the left side / first number is true, if not, update it as true, set event.id as the operator, and add it to the display.
    If the left side is already true, and a second number exists, it will calculate the first number, operator, and second number, appending this operator to the result for further calculations
    
    An equal button click will calculate the current calculation in the display, returning the result back to the display for further calculation, resetting operator and num2.
    If the calculation does not contain a num2, then it will simply return num1 as result, and display it for further calculations.
    
    Oh and I made the display look nicer (color diff between current and pastCalc, different fonts for buttons and display)

--- script.js
+++ script.js
@@ -1,19 +1,115 @@
 // operational variables
-let num1;
-let num2;
-let operator;
+let num1 = '';
+let num2 = '';
+let operator = '';
 
+let result;
+
+let leftSide = false;
+let rightSide = false;
+let operated = false;
+
+let isError = false;
+
+// list of numbers and operators
+const NUMLIST = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const OPLIST = ['+', '-', '×', '÷', '%'];
+
+// DOM variables
+const currentDisplay = document.getElementById('current');
+const pastCalc = document.getElementById('pastCalc');
+
+const calcBtns = document.getElementById('buttonGrid');
+
+
+// Link to buttons
+calcBtns.addEventListener('click', (event) => {
+
+    if(!event.target.closest('button')) return;
+
+    if (isError && !isClearButton(event)) {
+        return;
+    }
+
+    // Number buttons
+    if (NUMLIST.includes(event.target.textContent) && leftSide == false) {
+        num1 += event.target.id;
+        currentDisplay.textContent += event.target.id;
+    } else if (NUMLIST.includes(event.target.textContent) && leftSide == true) {
+        num2 += event.target.id;
+        currentDisplay.textContent += event.target.id;
+    }
+
+    // Operator buttons
+    if (OPLIST.includes(event.target.textContent) && leftSide == false) {
+        leftSide = true;
+        operator = event.target.id;
+        currentDisplay.textContent += event.target.id;
+    } else if (OPLIST.includes(event.target.textContent) && leftSide == true) {
+        if (operator !== '') {
+            // complete operation, move to pastcalc
+            result = operate(Number(num1), operator, Number(num2));
+            pastCalc.textContent = currentDisplay.textContent;
+            currentDisplay.textContent = result;
+
+            // reset for second calculation
+            num1 = result;
+            num2 = '';
+            operator = event.target.id;
+            currentDisplay.textContent += event.target.id;
+        } else {
+            currentDisplay.textContent += event.target.id;
+        }
+    }
+
+    // Equals button
+    if (event.target.textContent === '=') {
+        if (leftSide === false) {
+            if (num1 === '') {
+                pastCalc.textContent = 0;
+                currentDisplay.textContent = '';
+            } else {
+                pastCalc.textContent = num1;
+            }
+        }
+        result = operate(Number(num1), operator, Number(num2));
+        if (isNaN(result) || result === Infinity) {
+            currentDisplay.textContent = 'ERROR';
+            isError = true;
+            return;
+        } else {
+            pastCalc.textContent = currentDisplay.textContent;
+            currentDisplay.textContent = result;
+
+            // reset for second calculation
+            num1 = result;
+            num2 = '';
+            operator = '';
+            leftSide = false;
+        }
+    }
+})
+
+// display buttons
+function displayNum(i) {
+    current.textContent += i;
+    if (current.textContent == 0) {
+        a = i;
+    } else if (current.textContent) {
+
+    }
+}
 
 // operate function
 function operate(a, operator, b) {
-    if (operator = add) {
-        add(a, b);
-    } else if (operator = subtract) {
-        subtract(a, b);
-    } else if (operator = multiply) {
-        multiply(a, b);
-    } else if (operator = divide) {
-        divide (a, b);
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '×') {
+        return multiply(a, b);
+    } else if (operator === '÷') {
+        return divide (a, b);
     }
 }
 
@@ -35,3 +131,9 @@ console.log(add(6, 7));
 console.log(subtract(6, 7));
 console.log(multiply(6, 7));
 console.log(divide(6, 7));
+
+
+// TO WORK ON
+function clearDisplay() {
+
+}
